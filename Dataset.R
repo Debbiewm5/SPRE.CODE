b6a225f63d7e5a1f917c6e1a646be1cd72e964c3
Deborah Weissman-Miller <dwmstat@gmail.com>
2017-09-28 11:48:09 -0600

    Dataset for Windows computers

--- Dataset.R
+++ Dataset.R
@@ -1,6 +1,6 @@
 Dataset <- function() {
     Dataset <<- read.csv("~/HEAT_LILLY_Heat.Stat.14_2016.csv")
-
+##  Insert your Dataset name in ("~/HEAT_LILLY_Heat.Stat.14_2016.csv")
     return(list(Dataset))
 }
 foo <- Dataset()
